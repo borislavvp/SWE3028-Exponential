d54fe785e57fa0dce42888cfb916a95f52b6cbda
Borislav Pavlov <416657@student.fontys.nl>
2021-10-12 02:18:50 +0900

    initial setup for the identity api solution

---
+++ identity/identity/Controllers/IdentityController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IdentityController : ControllerBase
+    {
+
+        private readonly ILogger<IdentityController> _logger;
+
+        public IdentityController(ILogger<IdentityController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ identity/service/IdentityService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Identity.Service
+{
+    public class IdentityService
+    {
+    }
+}
