106556a8ad39aaee769a0a1cbadf7d3bdae61f20
Borislav Pavlov <416657@student.fontys.nl>
2021-10-27 20:36:06 +0900

    defined the token and identity services

---
+++ identity/service/Interfaces/IResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Service.Interfaces
+{
+    public interface IResult
+    {
+        string FailureReason { get; }
+
+        bool IsSuccess { get; }
+    }
+
+    public interface IResult<T> : IResult
+    {
+        T Data { get; }
+    }
+}
