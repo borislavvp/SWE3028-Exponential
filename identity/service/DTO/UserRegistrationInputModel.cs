55fb1c22e28e47cb6bbd241cfda3530ee7fc249f
Borislav Pavlov <416657@student.fontys.nl>
2021-10-27 20:35:51 +0900

    added input models

---
+++ identity/service/DTO/UserRegistrationInputModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Service.DTO
+{
+    public class UserRegistrationInputModel
+    {
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
