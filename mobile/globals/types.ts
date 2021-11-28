3b2371ba3b4ab809768f7c5389d1cfe5636e5539
Borislav Pavlov <416657@student.fontys.nl>
2021-11-29 03:21:49 +0900

    added notification item screen

--- mobile/globals/types.ts
+++ mobile/globals/types.ts
@@ -19,6 +19,7 @@ export type RootStackParamList = {
   NotFound: undefined;
   Register: undefined;
   StockItem: undefined;
+  StockNotificationItem: undefined;
 };
 
 export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
