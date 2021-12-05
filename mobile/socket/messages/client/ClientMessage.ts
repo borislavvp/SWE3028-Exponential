1e25e1b43f6ee92bc4720ec177fb3bae479f39cf
Borislav Pavlov <416657@student.fontys.nl>
2021-12-05 15:45:48 +0900

    removed the socket and fixed issue in the notifications reducer

--- mobile/socket/messages/client/ClientMessage.ts
+++
@@ -1,3 +0,0 @@
-import { SetAlertMessage } from "./SetAlertMessage";
-
-export type ClientMessage = SetAlertMessage
--- mobile/socket/messages/server/ServerMessage.ts
+++
@@ -1,4 +0,0 @@
-import { StockItemChangeMessage } from "./StockItemChangeMessage";
-import { SupportedStocksMessage } from "./SupportedStocksMessage";
-
-export type ServerMessage = StockItemChangeMessage | SupportedStocksMessage
