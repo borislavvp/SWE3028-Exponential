3fed9fa5ff4a05ac2634086f313db4c4697be8c0
Borislav Pavlov <416657@student.fontys.nl>
2021-12-09 01:37:50 +0900

    added stocks api

--- mobile/redux/actions/stocks/api/stocksAPI.ts
+++ mobile/redux/actions/stocks/api/stocksAPI.ts
@@ -1,30 +1,46 @@
-import axios, { AxiosResponse } from 'axios';
-import { DateTime, Duration } from 'luxon';
-import { AuthState } from '../../../reducers/auth/types/AuthState';
-import { StockSearchResult } from '../types/SearchStock';
+import axios, { AxiosResponse } from "axios";
+import { StockSearchResult } from "../types/SearchStock";
 
 const base = "https://api.polygon.io/v3/reference";
+const API_KEY = "API_KEY";
 
-const stockLogo = (stockSymbol:string) => `https://s3.polygon.io/logos/${stockSymbol.toLocaleLowerCase()}/logo.png`;
+export const stockLogo = (stockSymbol: string) => `https://s3.polygon.io/logos/${stockSymbol.toLocaleLowerCase()}/logo.png`;
 
-const caller = axios.create({ baseURL: base })
+const caller = axios.create({ baseURL: base });
 
 export const stocksAPI = {
-  searchStock(stockName:string): Promise<StockSearchResult[]> {
-    return new Promise((resolve,reject) => {
-        caller
-            .get(`${base}/tickers?search=${stockName}&active=true&sort=ticker&order=asc&limit=10&apiKey=9XKAPSk7sd5zA_ebxUzgHixBQU5BmDAJ` )
-            .then((res: AxiosResponse<{ results: { ticker: string, name: string }[]}>) => {
-                const data = [
-                    ...res.data.results.map(s => ({
-                        ticker: s.ticker,
-                        name: s.name,
-                        logo: stockLogo(s.ticker)
-                    } as StockSearchResult))
-                ]
-                resolve(data);
-            })
-            .catch(() => reject());
+  searchStock(stockName: string): Promise<StockSearchResult[]> {
+    return new Promise((resolve, reject) => {
+      caller
+        .get(`${base}/tickers?search=${stockName}&active=true&sort=ticker&order=asc&limit=10&apiKey=${API_KEY}`)
+        .then((res: AxiosResponse<{ results: { ticker: string; name: string }[] }>) => {
+          const data = [
+            ...res.data.results.map(
+              (s) =>
+                ({
+                  ticker: s.ticker,
+                  name: s.name,
+                  logo: stockLogo(s.ticker),
+                } as StockSearchResult)
+            ),
+          ];
+          resolve(data);
+        })
+        .catch((e) => {
+          
+          console.log(e);
+          reject()
+        });
+    });
+  },
+  stockHistoricalData(stockName: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      caller
+        .get(`http://api.marketstack.com/v1/tickers/${stockName}/eod?access_key=${API_KEY}&limit=10`)
+        .then((res: AxiosResponse<{ data: { eod: [] } }>) => {
+          resolve(res.data.data.eod);
+        })
+        .catch(() => reject());
     });
   },
 };
